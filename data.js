c2f14034e7b33f8f0d848def82aa65063d39fe50
kasnyi <69011884+kasnyi@users.noreply.github.com>
2023-01-12 21:21:41 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,134 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-new-project",
+      "name": "New Project",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": -0.09616305291989136,
+        "pitch": -0.14762114870060827,
+        "fov": 1.384244232961148
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8098678426514123,
+          "pitch": -0.24859310731006445,
+          "rotation": 0,
+          "target": "1-new-project-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5421422684318458,
+          "pitch": -0.42559575004806227,
+          "title": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" dir=\"ltr\" style=\"unicode-bidi: isolate; font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin-top: -2px; margin-bottom: -2px; resize: none; font-family: inherit; overflow: hidden; width: 270px; white-space: pre-wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><span class=\"Y2IQFc\" lang=\"my\">တည်နေရာ</span></pre>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.5106960343153819,
+          "pitch": -0.16355587128409965,
+          "title": "<p class=\"p1\" style=\"font: 400 13px &quot;Noto Sans Myanmar&quot;; color: rgb(0, 0, 0);\">တည်နေရာ</p>",
+          "text": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" dir=\"ltr\" style=\"unicode-bidi: isolate; font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin-top: -2px; margin-bottom: -2px; resize: none; font-family: inherit; overflow: hidden; width: 270px; white-space: pre-wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" dir=\"ltr\" style=\"overflow: hidden; font-family: inherit; font-size: 28px; unicode-bidi: isolate; line-height: 36px; border: none; padding: 2px 0.14em 2px 0px; position: relative; margin-top: -2px; margin-bottom: -2px; resize: none; width: 270px; white-space: pre-wrap; overflow-wrap: break-word;\"><span class=\"Y2IQFc\" lang=\"my\">မင်္ဂလားပါ</span></pre></pre>"
+        },
+        {
+          "yaw": 0.12625569547227755,
+          "pitch": -0.588510144045145,
+          "title": "New Home Realty&nbsp;&nbsp;",
+          "text": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" dir=\"ltr\" style=\"unicode-bidi: isolate; font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin-top: -2px; margin-bottom: -2px; resize: none; font-family: inherit; overflow: hidden; width: 270px; white-space: pre-wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><span class=\"Y2IQFc\" lang=\"my\">မှကြိုဆိုပါတယ်။</span></pre>"
+        }
+      ]
+    },
+    {
+      "id": "1-new-project-1",
+      "name": "New Project (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": -0.777912585201797,
+        "pitch": 0.0482152384876553,
+        "fov": 1.384244232961148
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19019026236014192,
+          "pitch": -0.0785159017939634,
+          "rotation": 0,
+          "target": "2-new-project-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.10471224571324989,
+          "pitch": 0.32677564765866585,
+          "title": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" dir=\"ltr\" style=\"unicode-bidi: isolate; font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin-top: -2px; margin-bottom: -2px; resize: none; font-family: inherit; overflow: hidden; width: 270px; white-space: pre-wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><span class=\"Y2IQFc\" lang=\"my\">စျေးနှုန်း</span></pre>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-new-project-2",
+      "name": "New Project (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.37649245293020606,
+        "pitch": 0.1274055637302549,
+        "fov": 1.384244232961148
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.42435963077249994,
+          "pitch": 0.03229911695823873,
+          "rotation": 0,
+          "target": "0-new-project"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16016268905375952,
+          "pitch": -0.18331185742757228,
+          "title": "<pre class=\"tw-data-text tw-text-large tw-ta\" data-placeholder=\"Translation\" id=\"tw-target-text\" dir=\"ltr\" style=\"unicode-bidi: isolate; font-size: 28px; line-height: 36px; background-color: rgb(48, 49, 52); border: none; padding: 2px 0.14em 2px 0px; position: relative; margin-top: -2px; margin-bottom: -2px; resize: none; font-family: inherit; overflow: hidden; width: 270px; white-space: pre-wrap; overflow-wrap: break-word; color: rgb(232, 234, 237);\"><span class=\"Y2IQFc\" lang=\"my\">မင်္ဂလားပါ</span></pre>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
